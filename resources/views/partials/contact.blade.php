dfecb6d5ecbb8ebaac5347238b2669ecd97f04ee
rhypnic <alfinindrawan54@gmail.com>
2021-02-26 13:35:35 +0700

    nih liat

---
+++ resources/views/partials/contact.blade.php
@@ -0,0 +1,36 @@
+<div class="row">
+
+    <div class="col d-flex align-items-stretch" >
+      <div class="info">
+        <div class="address">
+          <i class="icofont-google-map"></i>
+          <h4>Location:</h4>
+          <p color:#49505c;>A108 Adam Street, New York, NY 535022</p>
+        </div>
+
+        <div class="email">
+          <i class="icofont-envelope"></i>
+          <h4>Email:</h4>
+          <p color:#49505c;>info@example.com</p>
+        </div>
+
+        <div class="instagram">
+          <i class="icofont-instagram"></i>
+          <h4>instagram:</h4>
+          <a href="#" ><p>http</p></a>
+        </div>
+        <div class="youtube">
+            <i class="icofont-youtube"></i>
+            <h4>Youtube:</h4>
+            <a href="#" ><p>http</p></a>
+          </div>
+      
+      </div>
+     
+    </div>
+    <div class="col d-flex align-items-stretch" >
+        <iframe width="100%" height="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3957.6778157831973!2d112.79528841465802!3d-7.2774526735361516!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd7fa11b35fed03%3A0x9c97f605616e686b!2sITS+Robotics+Center!5e0!3m2!1sen!2sid!4v1559546399119!5m2!1sen!2sid" style="border:0" allowfullscreen=""></iframe>
+    </div>
+
+  </div>
+</div>
---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,28 @@
+<nav class="nav-menu d-none d-lg-block">
+  <ul>
+    <li class="active"><a href="index.html">Home</a></li>
+    <li><a href="#about">About Us</a></li>
+    <li><a href="#specialities">Specialities</a></li>
+    
+    <li><a href="#team">Team</a></li>
+    <li class="drop-down"><a href="">Drop Down</a>
+      <ul>
+        <li><a href="#">Drop Down 1</a></li>
+        <li class="drop-down"><a href="#">Deep Drop Down</a>
+          <ul>
+            <li><a href="#">Deep Drop Down 1</a></li>
+            <li><a href="#">Deep Drop Down 2</a></li>
+            <li><a href="#">Deep Drop Down 3</a></li>
+            <li><a href="#">Deep Drop Down 4</a></li>
+            <li><a href="#">Deep Drop Down 5</a></li>
+          </ul>
+        </li>
+        <li><a href="#">Drop Down 2</a></li>
+        <li><a href="#">Drop Down 3</a></li>
+        <li><a href="#">Drop Down 4</a></li>
+      </ul>
+    </li>
+    <li><a href="#contact">Contact</a></li>
+
+  </ul>
+</nav><!-- .nav-menu -->
